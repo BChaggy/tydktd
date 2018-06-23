ae1c66e39befdeebfe1f13cb652c986400242a28
BChaggy <40475759+BChaggy@users.noreply.github.com>
2018-06-23 18:54:13 -0400

    Add files via upload

---
+++ HTML/products.js
@@ -0,0 +1,15 @@
+function RunMenu() {
+  $('#productbutton1').click(function() {
+    window.location.href="dogproducts.html";
+  });
+
+  $('#productbutton2').click(function() {
+    window.location.href="catproducts.html";
+  });
+
+  $('#productbutton3').click(function() {
+    window.location.href="snakeproducts.html";
+  });
+};
+
+RunMenu();
